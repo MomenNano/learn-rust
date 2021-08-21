6a61c6e78eb2249383c0fec04dc2d804a570a2db
Momen <momen@afnan-tech.com>
2021-08-21 19:12:37 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let guess: i32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Large!"),
+        Ordering::Equal => println!("Winner!"),
+    }
+    
+}
